f4144be9a4cc7d52280c1f0f7af5dbeb40d1b465
Jonathan Muñoz <jonathan2712emr@gmail.com>
2020-06-14 01:44:28 -0500

    Carga de la imagen del empleado seleccionado incorporada
    Cuando el usuario selecciona un empleado de la lista, se carga su fotografía en la interfaz.

--- src/app/frm-main/frm-main.page.ts
+++ src/app/frm-main/frm-main.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from '../services/auth.service';
 import { empleado } from '../shared/usuario.class';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-frm-main',
@@ -11,11 +12,12 @@ import { empleado } from '../shared/usuario.class';
 })
 export class FrmMainPage implements OnInit {
   listaDeEmpleados = [];
-
+  imagenDelEmpleado = 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Google_Camera_Icon.svg';
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth,
-    private fireSvc: AuthService
+    private fireSvc: AuthService,
+    private alertCon: AlertController
   ) {}
 
   ngOnInit() {
@@ -42,5 +44,16 @@ export class FrmMainPage implements OnInit {
   onSalir() {
     this.afAuth.auth.signOut();
     this.router.navigateByUrl('/home');
+  }
+
+  cargarImagen(evento) {
+    this.listaDeEmpleados.forEach((empleado) => {
+      if (
+        empleado['nombre'] + ' ' + empleado['apellido'] ===
+        evento.target.value.trim()
+      ) {
+        this.imagenDelEmpleado = `${empleado['fotoUrl']}`;
+      }
+    });
   }
 }
