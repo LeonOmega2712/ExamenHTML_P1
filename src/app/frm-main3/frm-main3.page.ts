15d049182c4f3495ed251089070f80d7d5276252
Jonathan Muñoz <jonathan2712emr@gmail.com>
2020-06-14 21:12:44 -0500

    Se agregó chart estático
    Se agregó un chart estático a la pestaña de reportes

--- src/app/frm-main3/frm-main3.page.ts
+++ src/app/frm-main3/frm-main3.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from '../services/auth.service';
-import { usuario } from '../shared/usuario.class';
+import { empleado } from '../shared/usuario.class';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-frm-main3',
@@ -10,23 +11,53 @@ import { usuario } from '../shared/usuario.class';
   styleUrls: ['./frm-main3.page.scss'],
 })
 export class FrmMain3Page implements OnInit {
-  uid;
-  u: usuario = new usuario();
-
+  @ViewChild('barCanvas', null) barCanvas: ElementRef;
+  barChart: Chart;
   constructor(private router: Router, private afAuth: AngularFireAuth, private fireSvc: AuthService) {}
-
+  
   ngOnInit() {
-    // CONSEGUIR EL UID DEL USUARIO Y QUITARLE LAS COMILLAS DE JSON.STRINGIFY
-    this.uid = localStorage.getItem('uid');
-    this.uid = this.uid.replace('"', '');
-    this.uid = this.uid.replace('"', '');
-
-    // COMRPOBAR EN FIREBASE SI EL USUARIO ACTUAL TIENE NOMBRE RESGITRADO
-    this.fireSvc.getComodin(this.uid, 'usuarios').subscribe((result) => {
-      if (result.payload.get('nombre') !== undefined) {
-        this.u.nombre = result.payload.data()['nombre'];
-      } else {
-        this.u.nombre = '----------';
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false,
+        },
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
       }
     });
   }
